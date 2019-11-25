c7da8412c678cec61704cab1604e734e1a337d20
KuramaMD <cicati9@gmail.com>
2019-11-25 13:41:44 +0200

    Add files via upload

--- worker/desenare.js
+++ worker/desenare.js
@@ -1,11 +1,11 @@
-document.getElementById("id_logic").innerHTML = "2019.11.25.5";
+document.getElementById("id_logic").innerHTML = "2019.11.25.6";
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
 
 var timer_id;
 var unghi = {};
 unghi.valoare = 0;
-
+var muncitor = null;
 function desenare(unghi) {
     var canvas = document.getElementById("id_canvas");
     var context = canvas.getContext("2d");
@@ -32,11 +32,16 @@ function start() {
     document.getElementById("id_stop").disabled = false;
 
     timer_id = setInterval(desenare, 20, unghi);
+    if (muncitor == null){
 
-    var muncitor = new Worker("prim.js");
+    muncitor = new Worker("prim.js");
     muncitor.onmessage = function(e){
         document.getElementById("id_prim").innerHTML = e.data;
+     }
+     muncitor.postMessage("start");
     }
+    else 
+    muncitor.postMessage("start");
 }
 
 function stop() {
@@ -44,4 +49,5 @@ function stop() {
     document.getElementById("id_stop").disabled = true;
 
     clearInterval(timer_id);
+    muncitor.postMessage("stop")
 }
