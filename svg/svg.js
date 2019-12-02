f202e811273ba138f1f7cff1277afae0c3bb9bdd
KuramaMD <cicati9@gmail.com>
2019-12-02 13:25:33 +0200

    Add files via upload

--- svg/svg.js
+++ svg/svg.js
@@ -1,4 +1,4 @@
-deseneazaocument.getElementById("id_logic_version").innerHTML="Logic = 2019-11-04.1";
+document.getElementById("id_logic_version").innerHTML="Logic = 2019-11-04.1";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
